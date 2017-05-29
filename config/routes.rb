97262239d3b68f8f4a15c164b5412fcc8383fe25
Kierachell <artemavovk@gmail.com>
2017-05-29 08:33:32 -0400

    yarn; react_on_rails boilerplate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   get "/sprint/standup" => "sprint#standup"
